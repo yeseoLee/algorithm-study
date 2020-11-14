d145e4d866df01403e0cd256df1c17233c3b458b
이예서 <49704047+yeseoLee@users.noreply.github.com>
2020-11-15 02:32:17 +0900

    수학3 9375번 패션왕 신해빈

---
+++ PS/BOJ9375.py
@@ -0,0 +1,17 @@
+from collections import Counter
+import sys
+
+t=int(sys.stdin.readline())
+
+for i in range(t):
+    n=int(sys.stdin.readline())
+    kind=[]
+    for j in range(n):
+        a=sys.stdin.readline().split()
+        kind.append(a[1])
+    count=Counter(kind)
+    num=1
+    for item in count:
+        num*=count[item]+1
+    print(num-1)
+
