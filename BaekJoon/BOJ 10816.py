3e3c02bf386c745a60e781bbd09bb70a5a7de077
YeseoLee <lys7442@naver.com>
2021-02-01 20:06:41 +0900

    2/01 study

--- BaekJoon/BOJ 10816.py
+++ BaekJoon/BOJ 10816.py
@@ -1,3 +1,36 @@
+"""
+def BinaryLeft(arr,t,l,r):
+    if(l>r): return 0
+    m=(l+r)//2
+    if (arr[m]>t):
+        return BinaryLeft(arr,t,l,m-1)
+    if (arr[m]==t):
+        if(m==0 or arr[m-1]!=t): return m
+        return BinaryLeft(arr,t,l,m)
+    if (arr[m]<t):
+        return BinaryLeft(arr,t,m+1,r)
+    
+def BinaryRight(arr,t,l,r):
+    if(l>r): return 0
+    m=(l+r)//2
+    if (arr[m]>t):
+        return BinaryRight(arr,t,l,m-1)
+    if (arr[m]==t):
+        if(m==len(arr)-1 or arr[m+1]!=t): return m+1
+        return BinaryRight(arr,t,m+1,r)
+    if (arr[m]<t):
+        return BinaryRight(arr,t,m+1,r)
+
+import sys
+n=int(sys.stdin.readline())
+a=list(map(int,sys.stdin.readline().rstrip().split()))
+m=int(sys.stdin.readline())
+b=list(map(int,sys.stdin.readline().rstrip().split()))
+a=sorted(a)
+for i in b:
+    print(BinaryRight(a,i,0,n-1)-BinaryLeft(a,i,0,n-1))
+"""
+"""
 def BinarySearch(arr,t,l,r):
     if(l>r): return 0
     m=(l+r)//2
@@ -8,7 +41,6 @@ def BinarySearch(arr,t,l,r):
     if (arr[m]<t):
         return BinarySearch(arr,t,m+1,r)
 
-
 import sys
 n=int(sys.stdin.readline())
 a=list(map(int,sys.stdin.readline().rstrip().split()))
@@ -17,3 +49,4 @@ b=list(map(int,sys.stdin.readline().rstrip().split()))
 a=sorted(a)
 for i in b:
     print(BinarySearch(a,i,0,n-1))
+"""
