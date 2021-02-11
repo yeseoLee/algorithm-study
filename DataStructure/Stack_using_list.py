3b911e705f3d7c7322ec7e3c90925d733e1fd36b
이예서 <49704047+yeseoLee@users.noreply.github.com>
2021-02-11 23:48:06 +0900

    Update Stack_using_list.py

--- DataStructure/Stack_using_list.py
+++ DataStructure/Stack_using_list.py
@@ -11,28 +11,37 @@ class Stack:
         return str(self.top[::1])
 
     #구현함수
+    
+    #스택에 원소 삽입
     def push(self, item):
         if(len(self.pop)>=self.limit):
             self.top.append(item)
+    #스택 가장 위에 있는 원소를 삭제하고 반환
     def pop(self):
         if not self.isEmpty():
             return self.top.pop(-1)
         else:
             print("Stack underflow")
             exit()
+    #스택 가장 위에 있는 원소를 반환
     def peek(self):
         if not self.isEmpty():
             return self.top[-1]
         else:
             print("underflow")
             exit()
+    #스택을 비움
     def clear(self):
         self.top=[]
-    def isContain(selff, item) -> bool:
+    #스택 안에 특정 item이 포함되어 있는지를 bool값으로 반환
+    def isContain(self, item) -> bool:
         return item in self.top
+    #스택이 비어있는 지를 bool값으로 반환
     def isEmpty(self) -> bool :
         return len(self.top)==0
+    #스택이 가득 차 있는 지를 bool값으로 반환
     def isFull(self) -> bool :
         return self.size()==self.limit
-    def size(sellf) -> int :
+    #스택의 크기를 int 값으로 반환 
+    def size(self) -> int :
         return len(self.top)
