5e2bb2efa4e11e54d7c68424f184dab50cbc8f28
YeseoLee <lys7442@naver.com>
2021-02-21 15:37:25 +0900

    02/21 study

--- BaekJoon/BOJ 2178.py
+++ BaekJoon/BOJ 2178.py
@@ -1,8 +1,55 @@
-def DFS(x,y):
+from collections import deque
+def bfs():
+    que=deque([[0,0,1]])
+    while(que):
+        x,y,cnt=que.popleft()
+        print(f"x:{x},y:{y},cnt:{cnt}")
+        #(N,M)에 도착
+        if(x==m-1 and y==n-1):
+            print(cnt)
+            return -1
+        for dx,dy in zip(pmx,pmy):
+            curX,curY=x+dx,y+dy
+            #out of range
+            if(curX<0 or curX>=m or curY<0 or curY >=n):
+                continue        
+            #탐색 가능한 길을 찾음
+            if(arr[curY][curX]==1 and visited[curY][curX]==False):
+                que.append([curX,curY,cnt+1])
+                visited[curY][curX]=True
+ 
+n,m=map(int,input().split())
+arr=[list(map(int,input()))for i in range(n)]
+visited=[[False for row in range(m)] for col in range(n)]
+pmy=[0,+1,-1,0]
+pmx=[+1,0,0,-1]
+bfs()
+'''
+from collections import deque
+def dfs(x,y,cnt):
+    print(f"x:{x},y:{y},cnt:{cnt}")
+    #(N,M)에 도착
+    if(x==m-1 and y==n-1):
+        way.append(cnt)    
+    visited[y][x]=True
+    for dx,dy in zip(pmx,pmy):
+        curX,curY=x+dx,y+dy
+        #out of range
+        if(curX<0 or curX>=m or curY<0 or curY >=n):
+            continue        
+        #탐색 가능한 길을 찾음
+        if(arr[curY][curX]==1 and visited[curY][curX]==False):
+            dfs(curX,curY,cnt+1)      
+        
+n,m=map(int,input().split())
+arr=[list(map(int,input()))for i in range(n)]
+visited=[[False for row in range(m)] for col in range(n)]
+pmy=[0,+1,-1,0]
+pmx=[+1,0,0,-1]
+way=[]
 
-from sys import stdin
-n,m=map(int,stdin.readline().split())
-arr=[[stdin.readline().rstrip()] for _ in range(n)]
-
+dfs(0,0,1)
+print(min(way))
+'''
 
 
