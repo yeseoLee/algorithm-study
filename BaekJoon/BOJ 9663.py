60f685331f440ad9386cda0e88eb9e4fb2cb48c5
YeseoLee <lys7442@naver.com>
2021-03-07 22:31:05 +0900

    03/07 study

--- BaekJoon/BOJ 9663.py
+++ BaekJoon/BOJ 9663.py
@@ -1,3 +1,11 @@
+def dfs(arr):
+    
+    
 n=int(input())
 arr=[[0 for row in range(n)]for col in range(n)]
+count=0
 
+for i in range(n):
+    for j in range(n):
+        arr[i][j]=1
+        dfs(arr)
