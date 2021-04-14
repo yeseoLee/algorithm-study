0d1f378fefce5f7132e77bfdbe6f86cf73d73be2
YeseoLee <lys7442@naver.com>
2021-04-14 13:53:08 +0900

    Update BOJ 11401(못품).py

--- BaekJoon/BOJ 11401(못품).py
+++ BaekJoon/BOJ 11401(못품).py
@@ -1,26 +1,15 @@
-""" 이항 계수 문제
-#nCk = n!/((n-k)!*k!)
-1. 곱하는 횟수를 줄이기 nCk vs nC(n-k)
-
-n,k=map(int,input().split())
-result=1
-k= k if k<n-k else n-k
-for i in range(k):
-    result*=n//(i+1)
-    n+=-1
-print(result)
-
-2. 파스칼의 삼각형 분할정복 nCk = (n-1)C(k-1) + (n-1)Ck
-def nCk(n,k):
-    if(k==0 or n==1 or n==k):
-        return 1
+def power(a,b,c):
+    if b == 1:
+        return a % c
     else:
-        return nCk(n-1,k-1)+nCk(n-1,k)
+        temp = power(a, b // 2) # a^(b // 2)를 미리 구한다.
+        # b가 짝수 (a^(b//2)) * (a^(b//2)) = a^b
+        if b % 2 == 0:
+            return temp * temp % c 
+        # b가 홀수 (a^(b//2)) * (a^(b//2)) * a = a^b
+        else:    
+            return temp * temp * a % c 
 
-n,k=map(int,input().split())
-k= k if k<n-k else n-k
-result=nCk(n,k)
-print(result%1000000007)
-
-3.
-"""
+A,B,C= map(int, input().split())
+result = power(A,B,C)
+print(result)
