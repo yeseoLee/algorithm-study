8428e045f6142e2b8f655a5b680e7a75dcff58bb
YeseoLee <lys7442@naver.com>
2021-05-01 18:06:06 +0900

    04/30 study

--- Test.py
+++ Test.py
@@ -1,27 +1,16 @@
-def bfs(tx,ty):
-    while(que):
-        x,y=que.popleft()
-        v=visited[x][y]
-        if(x==tx and y==ty):
-            return v
-        for i in range(8):
-            xx=x+dx[i]
-            yy=y+dy[i]
-            if(0<=xx<=l-1 and 0<=yy<=l-1 and visited[xx][yy]==0):
-                que.append([xx,yy])
-                visited[xx][yy]=v+1
-        
-from collections import deque
-import sys
-t=int(sys.stdin.readline())
+def power(a,b,c):
+    if b == 1:
+        return a % c
+    else:
+        temp = power(a, b // 2) # a^(b // 2)를 미리 구한다.
+        # b가 짝수 (a^(b//2)) * (a^(b//2)) = a^b
+        if b % 2 == 0:
+            return temp * temp % c 
+        # b가 홀수 (a^(b//2)) * (a^(b//2)) * a = a^b
+        else:    
+            return temp * temp * a % c 
 
-for i in range(t):
-    l=int(sys.stdin.readline())
-    fx,fy=map(int,sys.stdin.readline().split()) #from
-    tx,ty=map(int,sys.stdin.readline().split()) #to
-    visited=[[0 for x in range(l)] for y in range(l)]
-    dx=[1,1,2,2,-1,-1,-2,-2]
-    dy=[2,-2,1,-1,2,-2,1,-1]
-    que=deque([[fx,fy]])
-    visited[fx][fy]=1
-    print(bfs(tx,ty)-1)
+A,B,C= map(int, input().split())
+result = power(A,B,C)
+print(result)
+
