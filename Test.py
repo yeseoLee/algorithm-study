6ac5a53db8de41d13a921553ea9ce91a47852081
이예서 <49704047+yeseoLee@users.noreply.github.com>
2021-05-23 17:06:17 +0900

    05/23 study

--- Test.py
+++ Test.py
@@ -1,23 +1,36 @@
-import sys
-inf=100000000
-def solve(s,e):
-    if(e-s==1):
-        dp[s][e]=sum(ch[s:e+1])
-        return
-    min_cost=inf
-    for bd in range(s,e):
-        if(s!=bd and dp[s][bd]==0):
-            solve(s,bd)
-        if((bd+1)!=e and dp[bd+1][e]==0):
-            solve(bd+1,e)
-        cost=dp[s][bd]+dp[bd+1][e]+sum(ch[s:e+1])
-        if(min_cost>cost):
-            min_cost=cost
-    dp[s][e]=min_cost
-t=int(sys.stdin.readline())
-for i in range(t):
-    k=int(sys.stdin.readline())
-    ch=list(map(int,sys.stdin.readline().split()))
-    dp=[[0 for i in range(k+1)] for j in range(k+1)]
-    solve(0,k)
-    print(dp[0][k-1])
+'''
+n,b=map(int,input().split())
+A=[list(map(int,input().split())) for i in range(n)]
+result=A.copy()
+tmp=[[0]*n for _ in range(n)]
+for i in range(b-1):
+    for j in range(n):
+        for k in range(n):
+            for x in range(n):
+                tmp[j][k]+=result[j][x]*A[x][k]
+            tmp[j][k]%=1000
+    result=tmp.copy()
+    tmp=[[0]*n for _ in range(n)]
+
+for i in result:
+    for j in i:
+        print(j,end=" ")
+    print()
+'''
+n,b=map(int,input().split())
+A=[list(map(int,input().split())) for i in range(n)]
+result=A.copy()
+tmp=[[0]*n for _ in range(n)]
+for j in range(n):
+    for k in range(n):
+        for x in range(n):
+            tmp[j][k]+=result[j][x]*A[x][k]
+        tmp[j][k]%=1000
+
+
+
+
+for i in result:
+    for j in i:
+        print(j,end=" ")
+    print()
