4582d9cf1c6b21c0e25d4ca1cf536e31103514ce
YeseoLee <lys7442@naver.com>
2021-07-02 14:33:16 +0900

    07/02 study

--- 스터디노트/SpiralArray.py
+++ 스터디노트/SpiralArray.py
@@ -55,3 +55,23 @@ cnt=0
 RIGHT(0,0)
 for x in matrix:
     print(x)
+
+'''
+X,Y = map(int,raw_input().split(' '))
+lis = [[-1 for i in xrange(Y)] for j in xrange(X)]
+x,y = 0,0
+dx,dy = 0,1
+count = 0
+while lis[x][y] == -1:
+    lis[x][y] = count
+    count+=1
+    x,y = x+dx,y+dy
+    if x in [-1,X] or y in [-1,Y] or lis[x][y] != -1:
+        x,y = x-dx,y-dy
+        dx,dy = dy,-dx
+        x,y = x+dx,y+dy
+for L in lis:
+    for val in L:
+        print '%3d'%val,
+    print
+'''
