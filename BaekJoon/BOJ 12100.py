7f0ae5f5fe1aed693cd42b570062aff15c17329d
YeseoLee <lys7442@naver.com>
2021-09-12 17:24:08 +0900

    09/12 study

--- BaekJoon/BOJ 12100.py
+++ BaekJoon/BOJ 12100.py
@@ -1,46 +1,80 @@
 import sys
+from collections import deque
 input = sys.stdin.readline # n x n
 
 n=int(input())
 board =[ list(map(int,input().split())) for _ in range(n)]
 
-# 4^5 = 2*10 = 1024
-
 def move_up():
     for j in range(n):
-        cant=-1 #already combined block
-        for i in range(1,n):
-            if board[i][j]==0:
-                pass
-            x,y = i-1,j
-            while (x>0 and board[x][y]==0):
-                x-=1
-            if x!=cant and board[x][y] == board[i][j]:
-                board[x][y] += board[i][j]
-                board[i][j] = 0
-                cant=x
-            elif board[x+1][y]==0:
-                board[x+1][y] = board[i][j]
-                board[i][j] = 0
+        que=deque([])
+        x=0
+        for i in range(n):
+            if board[i][j]!=0:
+                que.append(board[i][j])
+                board[i][j]=0
+        while(que):
+            tmp = que.popleft()
+            if que and tmp == que[0]:
+                tmp+=que.popleft()
+            board[x][j]=tmp
+            x+=1
+            
     for i in board:
         print(i)
             
 def move_down():
     for j in range(n):
-        cant=n #already combined block
-        for i in range(n-1,-1,-1):
-            if board[i][j]==0:
-                pass
-            x,y = i+1,j
-            while (x<n and board[x][y]==0):
-                x+=1
-            if x!=cant and board[x][y] == board[i][j]:
-                board[x][y] += board[i][j]
-                cant=x
-            else:
-                board[x-1][y] = board[i][j]
-            board[i][j] = 0
+        que=deque([])
+        x=n-1
+        for i in range(n):
+            if board[i][j]!=0:
+                que.append(board[i][j])
+                board[i][j]=0
+        while(que):
+            tmp = que.pop()
+            if que and tmp == que[-1]:
+                tmp+=que.pop()
+            board[x][j]=tmp
+            x-=1
+            
     for i in board:
         print(i)
 
-move_up()
+def move_left():
+    for i in range(n):
+        que=deque([])
+        x=0
+        for j in range(n):
+            if board[i][j]!=0:
+                que.append(board[i][j])
+                board[i][j]=0
+        while(que):
+            tmp = que.popleft()
+            if que and tmp == que[0]:
+                tmp+=que.popleft()
+            board[i][x]=tmp
+            x+=1
+            
+    for i in board:
+        print(i)
+
+def move_right():
+    for i in range(n):
+        que=deque([])
+        x=n-1
+        for j in range(n):
+            if board[i][j]!=0:
+                que.append(board[i][j])
+                board[i][j]=0
+        while(que):
+            tmp = que.pop()
+            if que and tmp == que[-1]:
+                tmp+=que.pop()
+            board[i][x]=tmp
+            x-=1
+            
+    for i in board:
+        print(i)
+
+
