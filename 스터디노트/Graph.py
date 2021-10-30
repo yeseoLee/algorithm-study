3d9803ab3bdff5e0f7ed5e740196392d548ccd6d
YeseoLee <lys7442@naver.com>
2021-10-31 03:02:26 +0900

    Create Graph.py

---
+++ 스터디노트/Graph.py
@@ -0,0 +1,28 @@
+def recursive_dfs(v,discovered=[]):
+    discovered.append(v)
+    for w in graph[v]:
+        if w not in discovered:
+            discovered = recursive_dfs(w,discovered)
+    return discovered
+
+def iterative_dfs(start_v):
+    discovered = []
+    stack = [start_v]
+    while stack:
+        v = stack.pop()
+        if v not in discovered:
+            discovered.append(v)
+            for w in graph[v]:
+                stack.append(v)
+    return discovered
+
+def iterative_bfs(start_v):
+    discovered = [start_v]
+    queue = [start_v]
+    while queue:
+        v = queue.pop(0)
+        for w in graph[v]:
+            if w not in discovered:
+                discovered.append(w)
+                queue.append(w)
+    return discovered
