d9431038172797c915630447dbed7a9f710c5024
YeseoLee <lys7442@naver.com>
2021-11-08 00:11:52 +0900

    11/07 study

--- BaekJoon/BOJ 1024.py
+++ BaekJoon/BOJ 1024.py
@@ -1,12 +1,19 @@
 import sys
 input=sys.stdin.readline
-n,l=map(int,input().split())
 
-if l%2==0:
-    if (n//l * 2 +1)*(l//2) == n:
-        return [x for x in range(n//l - l
-else:
-    if n%l==0:
-        return [x for x in range(n//l - l//2, n//l + l//2 +1)]
-    else:
+def get_sub(n,l):
+    if l>100:
         return -1
+    if l%2==0:
+        if (n//l * 2 +1)*(l//2) == n and n//l - l//2+1>=0:
+            return ' '.join(str(x) for x in range(n//l - l//2+1, n//l + l//2 + 1))
+        else:
+            return get_sub(n,l+1)
+    else:
+        if n%l==0 and n//l - l//2>=0:
+            return ' '.join(str(x) for x in range(n//l - l//2, n//l + l//2 +1))
+        else:
+            return get_sub(n,l+1)
+
+n,l=map(int,input().split())
+print(get_sub(n,l))
