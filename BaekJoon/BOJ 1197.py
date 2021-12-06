63a060cbb3faa5e207b11f36fe612cb08e329794
YeseoLee <lys7442@naver.com>
2021-12-06 17:14:40 +0900

    12/06 study

--- BaekJoon/BOJ 1197.py
+++ BaekJoon/BOJ 1197.py
@@ -1,15 +1,30 @@
 import sys
 import heapq
-imput=sys.stdin.readline
-
+input=sys.stdin.readline
 v,e=map(int,input().split())
-graph=[[] for _ in range(v+1)]
-visited=[False]*(v+1)
+elist=[]
 for i in range(e):
-    a,b,c=map(int,input().split())
-    graph[a].append((b,c))
-    graph[b].append((a,c))
+    elist.append(list(map(int,input().split())))
+elist.sort(key=lamda x : x[2])
+parent=[for i in range(1,v+1)]
 
+def union(a,b):
+    a = find(a)
+    b = find(b)
+    if b<a:
+        parent[a] = b
+    else:
+        parent[b] = a
 
-    
-    
+def find(a):
+    if a != parent[a]:
+        parent[a] = find(parent[a])
+    return parent[a]
+
+result = 0
+for s, e,w in elist:
+    if find(s) != find(e):
+        union(s,e)
+        result+=w
+
+print(result)
