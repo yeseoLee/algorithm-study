27bf53db1c7533207e7252de5d2f69360226ec04
YeseoLee <lys7442@naver.com>
2021-12-09 00:26:30 +0900

    12/08 study

--- BaekJoon/BOJ 2011.py
+++ BaekJoon/BOJ 2011.py
@@ -1,5 +1,15 @@
 import sys
 input = sys.stdin.readline
-n=list(input().strip())
+num=list(input().strip())
+dp=[0]*(len(num)+1)
+dp[0]=dp[1]=1
 
-dp=[0]
+if num[0] == '0':
+    print(0)
+else:
+    for i in range(2,len(num)+1):
+        if int(num[i-1])>0:
+            dp[i]+=dp[i-1]
+        if 10<= int(num[i-2])*10 + int(num[i-1])<=26:
+            dp[i]+=dp[i-2]
+    print(dp[len(num)] % 1000000)
