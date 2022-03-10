a8e355b8775c7c9a308cf8b0e12681da79f6eaef
YeseoLee <lys7442@naver.com>
2022-03-11 02:05:18 +0900

    03/11 study

--- BaekJoon/BOJ 16236.py
+++ BaekJoon/BOJ 16236.py
@@ -13,6 +13,7 @@ di,dj=[-1,0,0,+1],[0,-1,+1,0] # U,L,R,D
 
 def call_mom(time):
     print(time)
+    sys.exit(0)
 
 def eat_shark(fish_size):
     shark[2] -= fish_size
@@ -25,32 +26,31 @@ def bfs(i,j,t):
     visited=[[False for a in range(n)] for b in range(n)]
     que =deque([])
     que.append([i,j,t])
-    #visited[i][j]=True
+    if(i==0 and j==2):
+        print('*****HERE****')
+    visited[i][j]=True
     flag=0
     while(que):
         now_i, now_j, time = que.popleft()
         for k in range(4):
             next_i, next_j = now_i + di[k], now_j + dj[k]
+            print(next_i,next_j)
             if not (0<=next_i<n and 0<=next_j<n):
                 continue
             elif arr[next_i][next_j] > shark[0]:
                 continue
             elif 0 < arr[next_i][next_j] <shark[0]:
                 eat_shark(arr[next_i][next_j])
-                print(next_i,next_j)
                 arr[next_i][next_j]=0
-                time += 1
                 flag = 1
                 break
             elif not visited[next_i][next_j]:
                 visited[next_i][next_j]=True
                 que.append([next_i,next_j,time+1])
-    if(flag):
-        if shark[2]==0:
-            call_mom(time)
-        print(next_i,next_j)
-        bfs(next_i,next_j,time)
-        return
+        if (flag):
+            print(next_i,next_j,time+1,shark[0])
+            bfs(next_i,next_j,time+1)
+            break
     call_mom(t)
 
 for i in range(n):
