5df0d38a8f751a188b17c403012d44d29eb7a9b8
이예서 <49704047+yeseoLee@users.noreply.github.com>
2024-09-03 10:54:05 +0900

    BOJ 1197

--- BaekJoon/BOJ 1197.py
+++ BaekJoon/BOJ 1197.py
@@ -1,29 +1,40 @@
 import sys
-input=sys.stdin.readline
-v,e=map(int,input().split())
-elist=[]
-for i in range(e):
-    elist.append(list(map(int,input().split())))
-elist.sort(key=lambda x : x[2])
-parent=[i for i in range(v+1)]
 
-def union(a,b):
-    a = find(a)
-    b = find(b)
-    if b<a:
-        parent[a] = b
-    else:
-        parent[b] = a
+sys.setrecursionlimit(10**6)
+
+input = sys.stdin.readline
+
+V, E = map(int, input().split())
+
+graphs = []
+for _ in range(E):
+    s, e, c = map(int, input().split())
+    graphs.append((s, e, c))
+
+graphs.sort(key=lambda x: x[2])
+
+parent = [x for x in range(V + 1)]
+
 
 def find(a):
     if a != parent[a]:
         parent[a] = find(parent[a])
     return parent[a]
 
+
+def union(a, b):
+    a = find(a)
+    b = find(b)
+    if b < a:
+        parent[a] = b
+    else:
+        parent[b] = a
+
+
 result = 0
-for s, e,w in elist:
+for s, e, c in graphs:
     if find(s) != find(e):
-        union(s,e)
-        result+=w
+        union(s, e)
+        result += c
 
 print(result)
