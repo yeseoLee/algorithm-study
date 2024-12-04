8b146387480e02fcefbee5817995d1b8805e845c
이예서 <49704047+yeseoLee@users.noreply.github.com>
2024-12-04 12:35:46 +0900

    leetcode 344

---
+++ LeetCode/344.Reverse String.py
@@ -0,0 +1,12 @@
+# 0 ms / 22.5 MB
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        s.reverse()
+        # l,r = 0, len(s)-1
+        # while l<r:
+        #     s[l],s[r] = s[r],s[l]
+        #     l+=1
+        #     r-=1
