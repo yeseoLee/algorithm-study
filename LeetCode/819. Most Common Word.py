b626c71c60c71214a05a3e15c47b6c4d2f69c029
이예서 <49704047+yeseoLee@users.noreply.github.com>
2024-12-05 10:49:09 +0900

    leetcode 819

--- LeetCode/819. Most Common Word.py
+++ LeetCode/819. Most Common Word.py
@@ -1,27 +1,32 @@
-"""
-import re
 from collections import Counter
-def mostCommonWord(paragraph, banned):
-    words=list(re.sub(r'[^\w]', ' ', paragraph).lower().split())
-    counter=Counter()
-    for word in words:
-        if(word not in banned):
-            counter[word]+=1
-    print(counter)
-
-
-mostCommonWord("Bob hit a ball, the hit BALL flew far after it was hit.",["hit"])
-"""    
-
 import re
-from collections import Counter
+from types import List
+
+
 def mostCommonWord(paragraph, banned):
-    words= [word for word in re.sub(r'[^\w]', ' ', paragraph)
-        .lower().split() if word not in banned]
-    
-    counter=Counter(words)
+    words = [
+        word
+        for word in re.sub(r"[^\w]", " ", paragraph).lower().split()
+        if word not in banned
+    ]
+
+    counter = Counter(words)
     return counter.most_common(1)[0][0]
 
 
-print(mostCommonWord(
-    "Bob hit a ball, the hit BALL flew far after it was hit.", ["hit"]))
+print(
+    mostCommonWord("Bob hit a ball, the hit BALL flew far after it was hit.", ["hit"])
+)
+
+
+# 1 ms / 17.54 MB
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        paragraph = re.sub("[!?',;.]", " ", paragraph)
+        paragraph = paragraph.lower()
+        words = paragraph.split()
+
+        counter = Counter(words)
+        for word, _ in counter.most_common():
+            if word not in banned:
+                return word
