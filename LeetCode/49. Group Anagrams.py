089dd82e3c46105943581850f24f3939a4ed9e72
이예서 <49704047+yeseoLee@users.noreply.github.com>
2024-12-05 10:49:02 +0900

    leetcode 49

--- LeetCode/49. Group Anagrams.py
+++ LeetCode/49. Group Anagrams.py
@@ -1,26 +1,19 @@
-import collections
+from collections import defaultdict
+from types import List
+
+
 def groupAnagrams(strs):
-    anagrams = collections.defaultdict(list)
+    anagrams = defaultdict(list)
     for word in strs:
-        anagrams[''.join(sorted(word))].append(word)
+        anagrams["".join(sorted(word))].append(word)
     return list(anagrams.values())
 
-print(groupAnagrams(strs=["eat", "tea", "tan", "ate", "nat", "bat"]))
-"""
+
+# 11 ms / 20.23 MB
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        separate = []
-        for i in range(len(strs)):
-            if(strs[i] == -1):
-                continue
-            tmp = sorted(list(strs[i]))
-            x = [strs[i]]
-            for j in range(i+1, len(strs)):
-                if(strs[j] == -1):
-                    continue
-                if(sorted(list(strs[j])) == tmp):
-                    x.append(strs[j])
-                    strs[j] = -1
-            separate.append(x)
-        return separate
-"""
+        anagram_dict = defaultdict(list)
+        for s in strs:
+            anagram = "".join(sorted(list(s)))
+            anagram_dict[anagram].append(s)
+        return list(anagram_dict.values())
