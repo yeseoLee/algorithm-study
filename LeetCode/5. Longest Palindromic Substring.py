e0c03e4d45857a328b95f5ebad36f0a99cbeb21a
이예서 <49704047+yeseoLee@users.noreply.github.com>
2024-12-06 10:30:26 +0900

    leetcode 5

--- LeetCode/5. Longest Palindromic Substring.py
+++ LeetCode/5. Longest Palindromic Substring.py
@@ -1,37 +1,29 @@
-
-"""
+# 364 ms / 17.33 MB
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        max_substring = ''
-        len_max = 0
-        if(len(s) < 2 or s == s[::-1]):
-            return s
+        answer = s[0]
         for i in range(1, len(s)):
-            if(s[i-1] == s[i]):  # even
-                left = i-1
-                right = i
-                while(1):
-                    if(left < 0 or right >= len(s)):
-                        break
-                    if(right-left+1 > len_max and s[left:right+1] == s[left:right+1][::-1]):
-                        max_substring = s[left:right+1]
-                        len_max = right-left+1
-                    left -= 1
-                    right += 1
-            if(i+1 < len(s) and s[i-1] == s[i+1]):  # odd
-                left = i-1
-                right = i+1
-                while(1):
-                    if(left < 0 or right >= len(s)):
-                        break
-                    if(right-left+1 > len_max and s[left:right+1] == s[left:right+1][::-1]):
-                        max_substring = s[left:right+1]
-                        len_max = right-left+1
-                    left -= 1
-                    right += 1
-            else:
-                left, right = -1, -1
-        if(len_max == 0):
-            return s[0]
-        return max_substring
-"""
+            # 짝수
+            l, r = i - 1, i
+            while True:
+                if l < 0 or r >= len(s):
+                    break
+                if s[l] != s[r]:
+                    break
+                if len(answer) < r + 1 - l:
+                    answer = s[l : r + 1]
+                l -= 1
+                r += 1
+            # 홀수
+            l, r = i - 1, i + 1
+            while True:
+                if l < 0 or r >= len(s):
+                    break
+                if s[l] != s[r]:
+                    break
+                if len(answer) < r + 1 - l:
+                    answer = s[l : r + 1]
+                l -= 1
+                r += 1
+
+        return answer
