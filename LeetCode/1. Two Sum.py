192a4373f21dd1dc707db654e695affd6a879250
이예서 <49704047+yeseoLee@users.noreply.github.com>
2024-12-09 11:12:35 +0900

    leetcode 1

--- LeetCode/1. Two Sum.py
+++ LeetCode/1. Two Sum.py
@@ -1,34 +1,10 @@
-def twoSum(nums, target):
-    nums_sorted=sorted(nums)
-    i,j=0,len(nums)-1
-    min_i,max_j=i,j
-    i_idx,j_idx=-1,-1
-    while(1):
-        if(nums_sorted[i]+nums_sorted[j]<target):
-            i+=1
-            min_i=i
-            j=max_j
-        elif(nums_sorted[i]+nums_sorted[j]>target):
-            j-=1
-            max_j=j
-            i=min_i
-        else: #nums_sorted[i]+nums_sorted[j]==target
-            for idx,val in enumerate(nums):
-                if(val==nums_sorted[i] or val==nums_sorted[j]):
-                    if(i_idx==-1):
-                        i_idx=idx
-                    else:
-                        j_idx=idx
-                        return [i_idx,j_idx]
+from types import List
 
-'''
-def twoSum(nums, target):
-    for idx, val in enumerate(nums):
-        diff = target - val
-        if nums.count(diff)==1 and nums.index(diff) != idx:
-            return [idx, nums.index(diff)]
-        elif nums.count(diff)==2:
-            return [idx, nums.index(diff,idx+1)]
 
-print(twoSum([2,5,5,11],10))
-'''
+# 1763 ms / 17.86 MB
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
