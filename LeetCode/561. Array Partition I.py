6e7021db409331b65438205896489fe2f74a5e6d
이예서 <49704047+yeseoLee@users.noreply.github.com>
2024-12-11 11:38:11 +0900

    leetcode 561

--- LeetCode/561. Array Partition I.py
+++ LeetCode/561. Array Partition I.py
@@ -1,3 +1,16 @@
+from typing import List
+
+
 class Solution:
     def arrayPairSum(self, nums: List[int]) -> int:
         return sum(sorted(nums)[::2])
+
+
+# 33 ms / 19.25 MB
+class Solution2:
+    def arrayPairSum(self, nums: List[int]) -> int:
+        result = 0
+        for idx, val in enumerate(sorted(nums)):
+            if idx % 2 == 0:
+                result += val
+        return result
