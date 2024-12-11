5ced1cd14b2d121034feddedd41a1d2aec038099
이예서 <49704047+yeseoLee@users.noreply.github.com>
2024-12-11 11:38:05 +0900

    leetcode 15

--- LeetCode/15. 3Sum.py
+++ LeetCode/15. 3Sum.py
@@ -1,28 +1,21 @@
+from types import List
+
+
+# 1765 ms / 178.62 MB
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        results=[]
-        nums.sort()
-        
-        for i in range(len(nums) -2):
-            #중복 건너뛰기
-            if i > 0 and nums[i] == nums[i-1]:
-                continue
-            left,right = i+1, len(nums)-1
-            while left<right:
-                sum = nums[i]+nums[left]+nums[right]
-                if sum<0:
-                    left+=1
-                elif sum>0:
-                    right-=1
+        answer = []
+        nums = sorted(nums)
+        for i in range(len(nums) - 2):
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                s = nums[l] + nums[r] + nums[i]
+                if s < 0:
+                    l += 1
+                elif s > 0:
+                    r -= 1
                 else:
-                    results.append([nums[i],nums[left],nums[right]])
-                    
-                    #중복인 값 전부 넘기고
-                    while left<right and nums[left] == nums[left+1]:
-                        left+=1
-                    while left<right and nums[right] == nums[right-1]:
-                        right-=1
-                    #범위 좁히기
-                    left+=1
-                    right-=1
-        return results
+                    answer.append((nums[i], nums[l], nums[r]))
+                    l += 1
+                    r -= 1
+        return list(set(answer))
