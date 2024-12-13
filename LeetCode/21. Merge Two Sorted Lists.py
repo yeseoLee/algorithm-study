763571bda169f259e335d6b61fd86fa39dee545c
이예서 <49704047+yeseoLee@users.noreply.github.com>
2024-12-13 19:08:42 +0900

    leetcode 21

--- LeetCode/21. Merge Two Sorted Lists.py
+++ LeetCode/21. Merge Two Sorted Lists.py
@@ -1,37 +1,52 @@
+from typing import Optional
+
+
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+# 3ms / 17.31 MB
 class Solution:
-    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        l3=ListNode()
-        now=l3
-        n1,n2=l1,l2
-        while(1):
-            if n1==None and n2==None:
-                return l3.next
-            elif n2==None:
-                while(n1!=None):
-                    newnode=ListNode(n1.val)
-                    now.next=newnode
-                    now=now.next
-                    n1=n1.next
-            elif n1==None:
-                while(n2!=None):
-                    newnode=ListNode(n2.val)
-                    now.next=newnode
-                    now=now.next
-                    n2=n2.next
+    def mergeTwoLists(
+        self, list1: Optional[ListNode], list2: Optional[ListNode]
+    ) -> Optional[ListNode]:
+        if not list1:
+            return list2
+        if not list2:
+            return list1
+
+        node1 = list1
+        node2 = list2
+
+        merged_list = ListNode()
+        merged_node = merged_list
+
+        while node1 and node2:
+            if node1.val < node2.val:
+                merged_node.val = node1.val
+                node1 = node1.next
             else:
-                if n1.val < n2.val:
-                    newnode=ListNode(n1.val)
-                    now.next=newnode
-                    now=now.next
-                    n1=n1.next
-                else:
-                    newnode=ListNode(n2.val)
-                    now.next=newnode
-                    now=now.next
-                    n2=n2.next
-        return l3.next
+                merged_node.val = node2.val
+                node2 = node2.next
+            if node1 or node2:
+                merged_node.next = ListNode()
+                merged_node = merged_node.next
+
+        while node1:
+            merged_node.val = node1.val
+            node1 = node1.next
+            if node1:
+                merged_node.next = ListNode()
+                merged_node = merged_node.next
+
+        while node2:
+            merged_node.val = node2.val
+            node2 = node2.next
+            if node2:
+                merged_node.next = ListNode()
+                merged_node = merged_node.next
+
+        return merged_list
