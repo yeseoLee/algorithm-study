05890c0dfcae05b858eeb3e7555bc772391d7994
이예서 <49704047+yeseoLee@users.noreply.github.com>
2024-12-19 00:57:54 +0900

    leetcode 225

--- LeetCode/225. Implement Stack using Queues.py
+++ LeetCode/225. Implement Stack using Queues.py
@@ -1,54 +1,40 @@
-#1. Using 2 Queue
+from collections import deque
+
+
+# 3 ms / 17.88 MB
 class MyStack:
     def __init__(self):
-        self.que1=deque([])
-        self.que2=deque([])
+        self.que1 = deque()
+        self.que2 = deque()
 
     def push(self, x: int) -> None:
-        if self.que1:
-            self.que2.append(x)
-            while(self.que1):
-                self.que2.append(self.que1.popleft())
-        else :
-            self.que1.append(x)
-            while(self.que2):
-                self.que1.append(self.que2.popleft())
+        self.que1.append(x)
 
     def pop(self) -> int:
-        if self.que1:
-            return self.que1.popleft()
-        elif self.que2:
-            return self.que2.popleft()
-        else:
+        if self.empty():
             return None
-        
+        while True:
+            if len(self.que1) == 1:
+                target = self.que1.popleft()
+                break
+            self.que2.append(self.que1.popleft())
+        self.que1 = self.que2
+        self.que2 = deque()
+
+        return target
+
     def top(self) -> int:
-        if self.que1:
-            return self.que1[0]
-        elif self.que2:
-            return self.que2[0]
-        else:
+        if self.empty():
             return None
+        while True:
+            if len(self.que1) == 1:
+                target = self.que1.popleft()
+                self.que2.append(target)
+                break
+            self.que2.append(self.que1.popleft())
+        self.que1 = self.que2
+        self.que2 = deque()
+        return target
 
     def empty(self) -> bool:
-        return (not self.que1 and not self. que2)
-
-#2.  push x and sorting the others
-class MyStack:
-
-    def __init__(self):
-        self.que=collections.deque([])
-        
-    def push(self, x: int) -> None:
-        self.que.append(x)
-        for i in range(len(self.que)-1):
-            self.que.append(self.que.popleft())
-
-    def pop(self) -> int:
-        return self.que.popleft()
-        
-    def top(self) -> int:
-        return self.que[0]
-        
-    def empty(self) -> bool:
-        return not self.que
+        return len(self.que1) == 0
