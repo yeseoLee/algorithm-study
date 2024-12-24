9e5d058ee0597c5480fd68d7536734e109bd3d0a
이예서 <49704047+yeseoLee@users.noreply.github.com>
2024-12-24 16:06:29 +0900

    BOJ 2630

--- BaekJoon/BOJ 2630.py
+++ BaekJoon/BOJ 2630.py
@@ -1,28 +1,45 @@
 import sys
-global blue
-blue=0
 
-def s(p):
-    result=0
-    for i in p:
-        result+=sum(i)
-    return result
-def divide(p,n):
-    global blue
-    if(s(p)==0):
+input = sys.stdin.readline
+
+
+n = int(input())
+board = [list(map(int, input().split())) for _ in range(n)]
+
+
+def get_color(x, y, k):
+    blue = False
+    white = False
+    for i in range(x, x + k):
+        for j in range(y, y + k):
+            if board[i][j] == 1:
+                blue = True
+            else:
+                white = True
+            if blue and white:
+                return -1
+    if blue:
         return 1
-    elif(s(p)==n*n):
-        blue+=1
-        return 0
     else:
-        p1=[[p[i][j] for i in range(n//2)] for j in range(n//2)]
-        p2=[[p[i][j] for i in range(n//2,n)] for j in range(n//2)]
-        p3=[[p[i][j] for i in range(n//2)] for j in range(n//2,n)]
-        p4=[[p[i][j] for i in range(n//2,n)] for j in range(n//2,n)]
-        return divide(p1,n//2)+divide(p2,n//2)+divide(p3,n//2)+divide(p4,n//2)
+        return 0
 
-n=int(sys.stdin.readline())
-p=[list(map(int,sys.stdin.readline().split())) for i in range(n)]
 
-print(divide(p,n))
+white, blue = 0, 0
+
+
+def count_papers(x, y, k):
+    global white, blue
+    if get_color(x, y, k) == 0:
+        white += 1
+    elif get_color(x, y, k) == 1:
+        blue += 1
+    else:
+        count_papers(x, y, k // 2)
+        count_papers(x, y + k // 2, k // 2)
+        count_papers(x + k // 2, y, k // 2)
+        count_papers(x + k // 2, y + k // 2, k // 2)
+
+
+count_papers(0, 0, n)
+print(white)
 print(blue)
