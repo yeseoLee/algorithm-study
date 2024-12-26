543b15ff7dbbee52800bc25948f08205a5d77565
이예서 <49704047+yeseoLee@users.noreply.github.com>
2024-12-26 11:22:48 +0900

    leetcode 347

--- LeetCode/347. Top K Frequent Elements.py
+++ LeetCode/347. Top K Frequent Elements.py
@@ -1,12 +1,24 @@
+from typing import List
+from collections import Counter
+
+
+# 5 ms / 21.30 MB
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        counter = collections.Counter(nums)
-        result=[]
-        for val,cnt in counter.most_common():
+        counter = Counter(nums)
+        answer = [key for key, val in counter.most_common(k)]
+        return answer
+
+
+class Solution2:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        counter = Counter(nums)
+        result = []
+        for val, cnt in counter.most_common():
             result.append(val)
         return result[:k]
 
 
-class Solution:
+class Solution3:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         return list(zip(*Counter(nums).most_common(k)))[0]
