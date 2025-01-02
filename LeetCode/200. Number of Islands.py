73106284de8257b6b539ddb07bc56bd0cf7444f5
이예서 <49704047+yeseoLee@users.noreply.github.com>
2025-01-02 15:40:19 +0900

    leetcode 200

--- LeetCode/200. Number of Islands.py
+++ LeetCode/200. Number of Islands.py
@@ -1,19 +1,27 @@
+from typing import List
+
+
+# 248ms / 20.36MB
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        def dfs(vi,vj):
-            for i in range(4):
-                ni,nj = vi+di[i], vj+dj[i]
-                if 0<=ni<m and 0<=nj<n and grid[ni][nj]=='1':
-                    grid[ni][nj]='0'
-                    dfs(ni,nj)
-        
-        di,dj = [0,0,+1,-1], [+1,-1,0,0]
-        m,n=len(grid), len(grid[0])
-        result=0
-        
+        di, dj = [0, 0, 1, -1], [1, -1, 0, 0]
+
+        def _dfs(i, j):
+            grid[i][j] = "-1"
+            stack = [(i, j)]
+            while stack:
+                i, j = stack.pop()
+                for d in range(4):
+                    ni, nj = i + di[d], j + dj[d]
+                    if 0 <= ni < m and 0 <= nj < n and grid[ni][nj] == "1":
+                        stack.append((ni, nj))
+                        grid[ni][nj] = "-1"
+
+        cnt = 0
+        m, n = len(grid), len(grid[0])
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == '1':
-                    dfs(i,j)
-                    result+=1       
-        return result
+                if grid[i][j] == "1":
+                    _dfs(i, j)
+                    cnt += 1
+        return cnt
