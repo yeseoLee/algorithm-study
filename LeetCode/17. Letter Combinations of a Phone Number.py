823b2f31464ded3a961a31cb1c05e823308837cf
이예서 <49704047+yeseoLee@users.noreply.github.com>
2025-01-03 16:29:33 +0900

    leetcode 17

--- LeetCode/17. Letter Combinations of a Phone Number.py
+++ LeetCode/17. Letter Combinations of a Phone Number.py
@@ -1,31 +1,63 @@
+from typing import List
+
+
+# 0 ms / 18.05 MB
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        
-        def phone(num):         
-            if num==7 or num==9:
-                num= ord('a') + (num-2)*3 + num//9
-                return [chr(num),chr(num+1),chr(num+2),chr(num+3)]
+        table = {
+            "2": ["a", "b", "c"],
+            "3": ["d", "e", "f"],
+            "4": ["g", "h", "i"],
+            "5": ["j", "k", "l"],
+            "6": ["m", "n", "o"],
+            "7": ["p", "q", "r", "s"],
+            "8": ["t", "u", "v"],
+            "9": ["w", "x", "y", "z"],
+        }
+        answer = []
+
+        def _dfs(i, letters):
+            if i == len(digits):
+                answer.append("".join(letters))
+                return
+            for letter in table[digits[i]]:
+                letters.append(letter)
+                _dfs(i + 1, letters)
+                letters.pop()
+
+        if digits:
+            _dfs(0, [])
+        return answer
+
+
+class Solution2:
+    def letterCombinations(self, digits: str) -> List[str]:
+        def phone(num):
+            if num == 7 or num == 9:
+                num = ord("a") + (num - 2) * 3 + num // 9
+                return [chr(num), chr(num + 1), chr(num + 2), chr(num + 3)]
             else:
-                num= ord('a') + (num-2)*3 + num//8
-                return [chr(num),chr(num+1),chr(num+2)]
-            
-        def dfs(letters,idx):
-            if idx<len(digits):
-                num=int(digits[idx])
-                visited[num]=True
+                num = ord("a") + (num - 2) * 3 + num // 8
+                return [chr(num), chr(num + 1), chr(num + 2)]
+
+        def dfs(letters, idx):
+            if idx < len(digits):
+                num = int(digits[idx])
+                visited[num] = True
                 for letter in phone(num):
-                    dfs(letters + letter,idx+1)
-                visited[num]=False
-            if len(letters)!=0 and len(letters)==len(digits):
+                    dfs(letters + letter, idx + 1)
+                visited[num] = False
+            if len(letters) != 0 and len(letters) == len(digits):
                 result.append(letters)
-        
-        visited=[False]*10
-        result=[]
-        dfs("",0)
+
+        visited = [False] * 10
+        result = []
+        dfs("", 0)
         return result
 
-#모범답안
-class Solution:
+
+# 모범답안
+class Solution3:
     def letterCombinations(self, digits: str) -> List[str]:
         def dfs(index, path):
             # 백트래킹
@@ -35,15 +67,23 @@ class Solution:
 
             for i in range(index, len(digits)):
                 for j in dic[digits[i]]:
-                    dfs(i+1,path+j)
-        
+                    dfs(i + 1, path + j)
+
         if not digits:
             return []
-        
-        dic ={'2': 'abc', '3': 'def', '4':'ghi', '5':'jkl',
-             '6':'mno', '7': 'pqrs', '8':'tuv','9':'wxyz'}
-        
-        result =[]
-        dfs(0,"")
-        
+
+        dic = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz",
+        }
+
+        result = []
+        dfs(0, "")
+
         return result
