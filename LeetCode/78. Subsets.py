7e575312b24623246ed1f217c80215a2ef45f659
이예서 <49704047+yeseoLee@users.noreply.github.com>
2025-01-06 11:18:14 +0900

    leetcode 78

--- LeetCode/78. Subsets.py
+++ LeetCode/78. Subsets.py
@@ -1,11 +1,30 @@
+from typing import List
+
+
+# 0 ms / 17.76 MB
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        result=[]
-        
-        def dfs(subset,k):
-            for i in range(k,len(nums)):
-                dfs(subset+[nums[i]],i+1)
+        answer = []
+
+        def _dfs(start, tmp):
+            for i in range(start, len(nums)):
+                tmp.append(nums[i])
+                _dfs(i + 1, tmp)
+                tmp.pop()
+            answer.append(tmp[:])
+
+        _dfs(0, [])
+        return answer
+
+
+class Solution2:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result = []
+
+        def dfs(subset, k):
+            for i in range(k, len(nums)):
+                dfs(subset + [nums[i]], i + 1)
             result.append(subset)
-        
-        dfs([],0)
+
+        dfs([], 0)
         return result
