cae2d2212d33d0a2f8f3ed034506b7e6c9855d63
이예서 <49704047+yeseoLee@users.noreply.github.com>
2025-01-11 14:38:17 +0900

    leetcode 332

--- LeetCode/332. Reconstruct Itinerary.py
+++ LeetCode/332. Reconstruct Itinerary.py
@@ -1,31 +1,34 @@
-#재귀
 from collections import defaultdict
+from typing import List
+
+
+# 재귀
 class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
-        #define graph by defaultdict
-        graph=defaultdict(list)
-        for a,b in sorted(tickets, reverse=True):
-            graph[a].append(b)
-        
-        route = [] 
-        def dfs(a):
-            while graph[a]:
-                dfs(graph[a].pop())
-            route.append(a)
-                    
-        dfs("JFK")
+        graph = defaultdict(list)
+        for f, t in sorted(tickets, reverse=True):
+            graph[f].append(t)
+
+        route = []
+
+        def _dfs(cur_v):
+            while graph[cur_v]:
+                _dfs(graph[cur_v].pop())
+            route.append(cur_v)
+
+        _dfs("JFK")
         return route[::-1]
 
-#재귀를 반복으로
-from collections import defaultdict
-class Solution:
+
+# 재귀를 반복으로
+class Solution2:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
-        #define graph by defaultdict
-        graph=defaultdict(list)
-        for a,b in sorted(tickets):
+        # define graph by defaultdict
+        graph = defaultdict(list)
+        for a, b in sorted(tickets):
             graph[a].append(b)
-        
-        route,stack = [], ['JFK']
+
+        route, stack = [], ["JFK"]
         while stack:
             while graph[stack[-1]]:
                 stack.append(graph[stack[-1]].pop(0))
