9746183de1a2c5625a206147c6307adc1884daee
이예서 <49704047+yeseoLee@users.noreply.github.com>
2025-02-28 14:15:12 +0900

    leetcode 136

---
+++ LeetCode/136. Single Number.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        answer = 0
+        for num in nums:
+            answer ^= num
+
+        return answer
